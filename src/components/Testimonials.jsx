551f128b64151b11de6bf601976387d17250406f
Andy Newhouse <hi@andymnewhouse.me>
2023-04-20 18:21:42 -0500

    Update testimonials to use passed in data

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-export function Testimonials() {
+export function Testimonials({ testimonials }) {
     return (
         <section id="testimonials" className="bg-gray-100 py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -8,20 +8,22 @@ export function Testimonials() {
                 </div>
                 <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
                     <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
+                        {testimonials.map(testimonial => (
                         <div className="pt-8 sm:inline-block sm:w-full sm:px-4">
                             <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
                                 <blockquote className="text-gray-900">
-                                    <p>“Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.”</p>
+                                    <p>“{testimonial.quote}”</p>
                                 </blockquote>
                                 <figcaption className="mt-6 flex items-center gap-x-4">
                                     <img className="h-10 w-10 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                     <div>
-                                        <div className="font-semibold text-gray-900">Leslie Alexander</div>
-                                        <div className="text-gray-600">@lesliealexander</div>
+                                        <div className="font-semibold text-gray-900">{testimonial.title}</div>
+                                        <div className="text-gray-600">{testimonial.position}</div>
                                     </div>
                                 </figcaption>
                             </figure>
                         </div>
+                        ))}
                     </div>
                 </div>
             </div>
