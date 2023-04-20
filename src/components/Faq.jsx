8d1539df26d1fb2531f3428bf35cdc91fbd0b868
Andy Newhouse <hi@andymnewhouse.me>
2023-04-20 18:20:14 -0500

    Update faq to use passed in data

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,4 +1,4 @@
-export function Faq() {
+export function Faq({ faq }) {
     return (
         <section id="faq">
             <div className="mx-auto max-w-7xl divide-y divide-gray-900/10 px-6 py-24 sm:py-32 lg:py-40 lg:px-8">
@@ -7,12 +7,14 @@ export function Faq() {
                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We've answered all your burning questions. Hopefully.</p>
                 </div>
                 <dl className="mt-10 space-y-8 divide-y divide-gray-900/10">
+                    {faq.map(entry => (
                     <div className="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
-                        <dt className="text-base font-semibold leading-7 text-gray-900 lg:col-span-5">Is this a real app?</dt>
+                        <dt className="text-base font-semibold leading-7 text-gray-900 lg:col-span-5">{entry.title}</dt>
                         <dd className="mt-4 lg:col-span-7 lg:mt-0">
-                            <p className="text-base leading-7 text-gray-600">No this is a example Laravel application built by the lovely folks at Tighten to showcase various implementations of CoilPack for Expression Engine.</p>
+                            <p className="text-base leading-7 text-gray-600">{entry.description}</p>
                         </dd>
                     </div>
+                    ))}
                 </dl>
             </div>
         </section>
